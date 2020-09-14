659877ae0b925b02a7cf53f9de048fc3f202f7e1
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-14 15:00:31 -0700

    [bankaccount]added pin

--- bank_account.py
+++ bank_account.py
@@ -1,28 +1,49 @@
 class BankAccount():
-    def __init__(self, kind):
+    def __init__(self, kind, pin):
         self.kind = kind
         self.balance = 0
         self.overdraft_fees = 0
+        self.pin = pin
 
     def deposit(self, amount):
         self.balance += amount
 
-    def withdraw(self, amount):
-        self.balance -= amount
+    def withdraw(self, amount, pin_from_user):
 
-        if (self.balance < 0):
-            self.overdraft_fees +=36
+        if(self.pin == pin_from_user):
+            if(self.balance < amount):
+                print("sorry you dont have that amount in your account")
+                return 
+            elif (self.balance == amount):
+                self.balance -= amount
+                print("Current balance is now {}".format(self.balance))
+                return
+            else:
+                self.balance -= amount
+                print("Current balance is {}".format(self.balance))   
+                return
+        else: 
+            print("The pin number is incorrect")
+
+        # if (self.balance < 0):
+            # self.overdraft_fees +=36
         return amount  
+    def change_pin(self, pin):
+        self.pin = pin
+        print(f"The new pin number is {self.pin}")
 
-rome_checking = BankAccount("checking")
+
+rome_checking = BankAccount("checking", 1234)
 print("My new account is a {} account. ".format(rome_checking.kind)) 
 
 rome_checking.deposit(3000)
-print("My current balance is ${}.".format(rome_checking.balance))
+# print("My current balance is ${}.".format(rome_checking.balance))
 
-rome_checking.withdraw(1500)
-print("My current balance is ${}.".format(rome_checking.balance))
+rome_checking.withdraw(1500, 1555)
+# print("My current balance is ${}.".format(rome_checking.balance))
 
-rome_checking.withdraw(2000)
-print("Overdraft {}.".format(rome_checking.overdraft_fees))
-print("My current balance is {}.".format(rome_checking.balance))         
+rome_checking.withdraw(2000, 1234)
+# print("Overdraft {}.".format(rome_checking.overdraft_fees))
+# print("My current balance is {}.".format(rome_checking.balance))         
+
+rome_checking.change_pin(9987)
