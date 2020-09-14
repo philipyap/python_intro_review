14550cb8a20d85a71aa519c8717a6212a49099d0
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-14 14:44:54 -0700

    [bankaccount]added example

--- bank_account.py
+++ bank_account.py
@@ -5,13 +5,24 @@ class BankAccount():
         self.overdraft_fees = 0
 
     def deposit(self, amount):
-        self.balancet += amount
+        self.balance += amount
 
     def withdraw(self, amount):
         self.balance -= amount
 
         if (self.balance < 0):
             self.overdraft_fees +=36
-        return amount        
+        return amount  
 
-            
+rome_checking = BankAccount("checking")
+print("My new account is a {} account. ".format(rome_checking.kind)) 
+
+rome_checking.deposit(3000)
+print("My current balance is ${}.".format(rome_checking.balance))
+
+rome_checking.withdraw(1500)
+print("My current balance is ${}.".format(rome_checking.balance))
+
+rome_checking.withdraw(2000)
+print("Overdraft {}.".format(rome_checking.overdraft_fees))
+print("My current balance is {}.".format(rome_checking.balance))         
